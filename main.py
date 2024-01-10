84edcff81d26fbf3ce58be9eea58bb9915be1f1b
Ricardo Bene <ridabe@uol.com.br>
2024-01-10 02:06:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import requests
 from dotenv import dotenv_values
 from fastapi import FastAPI
-import sqlite3
 from pymongo import MongoClient
-import pymongo
 from datetime import datetime
 
 app = FastAPI()
@@ -12,7 +10,6 @@ env_vars = dotenv_values(".env")
 API_KEY = env_vars.get("API_KEY")
 DB_URL = env_vars.get("DATABASE_URL")
 BASE_URL = env_vars.get("API_URL")
-
 
 @app.get("/get_weather/{city}")
 # Função para buscar previsão do tempo
@@ -29,16 +26,11 @@ def get_weather_forecast(city):
     else:
         return {"city": city, "message": 'Erro ao obter previsão do tempo.', "data": None}
 
-
 def save_to_mongodb(city, response_data):
-    # client = pymongo.MongoClient("mongodb://localhost:27017/")  # Conectando ao MongoDB localmente
-    client = MongoClient(
-        'mongodb+srv://recrud:recrud2023bne@recrudprd.b82su.mongodb.net/recrud?retryWrites=true&w=majority')
+    client = MongoClient(DB_URL)
 
-    db = client['recrud']
+    db = client['raizen']
     collection = db['weather']
-    # db = client['weather_history']
-    # collection = db['weather']
 
     # Inserindo dados
     entry = {
@@ -49,22 +41,18 @@ def save_to_mongodb(city, response_data):
     collection.insert_one(entry)
     client.close()
 
-
 @app.get("/get_weather_database")
 def get_weather_database():
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    client = MongoClient(
-        'mongodb+srv://recrud:recrud2023bne@recrudprd.b82su.mongodb.net/recrud?retryWrites=true&w=majority')
+    client = MongoClient(DB_URL)
 
-    db = client['recrud']
+    db = client['raizen']
     collection = db['weather']
-
-    # # Selecionando o banco de dados
-    # db = client["weather_history"]
-    #
-    # # Selecionando a coleção
-    # collection = db["weather"]
-
     # Consulta para recuperar todos os documentos da coleção
     result = collection.find({})
-    return result
+    data = []
+    for doc in result:
+        if '_id' in doc:
+            doc.pop('_id')
+        data.append(doc)
+
+    return data
